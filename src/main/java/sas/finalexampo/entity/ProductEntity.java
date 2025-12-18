70438c93b28dbd8df1566d8c4568a76e73ac49b5
adiletdzholdasbai <adilet.dzholdasbai@narxoz.kz>
2025-12-18 15:54:56 +0500

    Add fields and JPA annotations to entity classes

--- src/main/java/sas/finalexampo/entity/ProductEntity.java
+++ src/main/java/sas/finalexampo/entity/ProductEntity.java
@@ -1,4 +1,30 @@
 package sas.finalexampo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String code;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "type_id")
+    private TypeEntity typeEntity;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "items_orders",joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "orderli_id"))    @Builder.Default
+    private List<OrderLineEntity> orders = new ArrayList<>();
 }
--- src/main/java/sas/finalexampo/entity/TypeEntity.java
+++ src/main/java/sas/finalexampo/entity/TypeEntity.java
@@ -1,4 +1,24 @@
 package sas.finalexampo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "types")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)    @Builder.Default
+    private List<AbstractReadWriteAccess.Item> items = new ArrayList<>();
 }
