f06db4447fc612dedb10fdae1db6beb81297e1ee
adiletdzholdasbai <adilet.dzholdasbai@narxoz.kz>
2025-12-18 15:47:12 +0500

    Add Type entity

--- src/main/java/sas/finalexampo/entity/TypeEntity.java
+++ src/main/java/sas/finalexampo/entity/TypeEntity.java
@@ -1,4 +1,23 @@
 package sas.finalexampo.entity;
 
-public class TypeEntity {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)    @Builder.Default
+    private List<Item> items = new ArrayList<>();
 }
