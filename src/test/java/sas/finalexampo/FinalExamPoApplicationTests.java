2602a8537f73373ababf9eee409ba13aa34e95f1
adiletdzholdasbai <adilet.dzholdasbai@narxoz.kz>
2025-12-18 15:12:05 +0500

    first commit

---
+++ src/test/java/sas/finalexampo/FinalExamPoApplicationTests.java
@@ -0,0 +1,13 @@
+package sas.finalexampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamPoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
